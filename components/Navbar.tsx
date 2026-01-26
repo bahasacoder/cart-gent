af1fd51aa7ab945c9b8ddfdb840f8715535cabac
bahasacoder <webcodebahasa@gmail.com>
2026-01-26 16:21:01 +0800

    menu nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,9 +11,13 @@ function Navbar() {
                     <h1>Shopping Cart</h1>
                 </div>
                 <ul>
-                    <li><Link href={"/"}>Home</Link></li>
-                    <li><Link href={"/fake-cart"}>Cart</Link></li>
-                    <li><Link href={"/fake-shop"}>Shop</Link></li>
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                                        <li><Link href={"/"}>Home</Link></li>
+                                        <li><Link href={"/fake-cart"}>Cart</Link></li>
+                                        <li><Link href={"/fake-shop"}>Shop</Link></li>
+                        </div>
+                    </div>
                 </ul>
             </nav>
         </header>
