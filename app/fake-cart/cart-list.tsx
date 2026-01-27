422bc5c9bda2ec43f816017905d615c5e65d5c9a
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-27 21:33:25 +0800

    Fix import statement syntax in cart-list.tsx
    Fix import statement syntax in cart-list.tsx

--- app/fake-cart/cart-list.tsx
+++ app/fake-cart/cart-list.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useDispatch, useSelector } from 'react-redux'; 
-import { store } from @/lib/store";
+import { store } from "@/lib/store";
 import { removeFromCart, updateQuantity, clearCart } from '@/lib/features/cartSlice';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
