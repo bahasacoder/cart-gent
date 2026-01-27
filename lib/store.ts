6df3cadfab523c6ade6f3fd0c9e0d5ec5b184fa6
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-27 21:41:47 +0800

    Update store.ts

--- lib/store.ts
+++ lib/store.ts
@@ -13,32 +13,18 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'
 // ... import your reducers
-import productReducer from "@/lib/features/productSlice"
+// import productReducer from "@/lib/features/productSlice"
 import cartReducer from "@/lib/features/cartSlice"
 
-const persistConfig = {
-  key: "cart",
-  storage: storage,
-};
-
-const rootReducers = combineReducers({
-  cart: cartReducer,
-  product: productReducer,
-});
-const persistedReducer = persistReducer(persistConfig, rootReducers)
 
 export const store = configureStore({
-   reducer:  persistedReducer , 
-    middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+   reducer:  {
+    cart: cartReducer
+   } ,
+ 
   });
 
   
-export const persistor = persistStore(store)
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
