49e8542a358bcb78c4ca79cd3a90408079265cca
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-28 09:52:16 +0800

    Add TypeScript types to StoreProvider component
    Add TypeScript types to StoreProvider component

--- lib/StoreProvider.tsx
+++ lib/StoreProvider.tsx
@@ -6,7 +6,8 @@ import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore } from './store'
 
-export default function StoreProvider({ children }) {
+export default function StoreProvider({ children }: {
+  children: React.ReactNode;}) {
   const storeRef = useRef()
   if (!storeRef.current) {
     storeRef.current = makeStore()
