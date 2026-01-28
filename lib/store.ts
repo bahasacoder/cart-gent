6f879d5a9a5159eb4e93f163b6eff4254712c572
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-28 10:06:06 +0800

    Enhance store.ts with type inference for store
    Added type inference for AppStore, RootState, and AppDispatch.

--- lib/store.ts
+++ lib/store.ts
@@ -12,5 +12,10 @@ export const makeStore = () => {
     },
   })
 }
+// export const store = makeStore()
 
-export const store = makeStore()
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
