444ea0ac05541d8aa070c4522213ad217e416b58
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-28 08:47:53 +0800

    pindah storeprovider
    pindah storeprovider

--- lib/StoreProvider.tsx
+++ lib/StoreProvider.tsx
@@ -4,7 +4,7 @@
 
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
-import { makeStore } from '@/lib/store'
+import { makeStore } from './store'
 
 export default function StoreProvider({ children }) {
   const storeRef = useRef()
