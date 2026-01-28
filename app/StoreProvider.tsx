65cf715dae482c808ab900683c217173cecea4cc
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-28 08:40:34 +0800

    Update import path for makeStore function

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -4,7 +4,7 @@
 
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
-import { makeStore } from './store'
+import { makeStore } from '@/lib/store'
 
 export default function StoreProvider({ children }) {
   const storeRef = useRef()
