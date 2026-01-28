ee579bcb6f0bc1cf3b6c954ad10cb8802d846aed
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-28 08:44:18 +0800

    Refactor store setup to use makeStore function
    Refactor store setup to use makeStore function

--- lib/store.ts
+++ lib/store.ts
@@ -1,31 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from '@reduxjs/toolkit'
-import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'
-// ... import your reducers
-// import productReducer from "@/lib/features/productSlice"
-import cartReducer from "@/lib/features/cartSlice"
+// /app/lib/store.js
 
+'use client'
 
-export const store = configureStore({
-   reducer:  {
-    cart: cartReducer
-   } ,
- 
-  });
+import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './features/cartSlice'
 
-  
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      cart: cartReducer,
+    },
+  })
+}
+
+export const store = makeStore()
